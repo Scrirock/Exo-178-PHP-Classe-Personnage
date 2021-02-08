d32b6ffaec7bf025d08f49907b0a5b3c189b498f
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-02-08 11:44:32 +0100

    fin exo

--- classes/Personnage.php
+++ classes/Personnage.php
@@ -4,10 +4,10 @@
 class Personnage {
 
     // La propriété id est privée, je ne peux pas la modifier ou la lire directement en dehors de cette classe
-    private $id;
-    public $nom;
-    public $x;
-    public $y;
+    private Int $id;
+    private String $nom;
+    public Int $x;
+    public Int $y;
 
     // Constructeur de ma classe permettant de définir des valeurs par défaut lorsqu'elle est instanciée
     public function __construct() {
@@ -22,6 +22,21 @@ class Personnage {
 
     }
 
+    public function walkLeft() {
+        $this->x += -1;
+
+    }
+
+    public function walktop() {
+        $this->y += 1;
+
+    }
+
+    public function walkbottom() {
+        $this->y += -1;
+
+    }
+
     // Setter permettant de définir l'attribut privé id
     public function setId($id) {
         $this->id = $id;
@@ -31,4 +46,24 @@ class Personnage {
     public function getId() {
         return $this->id;
     }
+
+    /**
+     * Return the name of the personnage
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the name of the personnage
+     * @param string $nom
+     */
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+
 } 
